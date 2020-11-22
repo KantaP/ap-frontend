5b9a296ced7abf974cfd02da6c94b5cd7959f946
Hide Mac <kantapong@doctoratoz.co>
2020-11-22 15:28:16 +0700

    update function

--- src/Booking.test.js
+++ src/Booking.test.js
@@ -68,8 +68,30 @@ const bookingData = [
       "startTime": "2019-10-04 14:00:00",
       "endTime": "2019-10-06 11:00:00",
       "title": "3-day workshop Corgi costume"
+    },
+    {
+        "id": 11,
+        "roomId": "A101",
+        "startTime": "2020-11-21 14:00:00",
+        "endTime": "2020-11-21 18:00:00",
+        "title": "Current date busy"
     }
 ]
+
+const lastMonday = (dateObject) => {
+    const today = dateObject.getDate();
+    const dayOfTheWeek = dateObject.getDay();
+    const newDate = dateObject.setDate(today - (dayOfTheWeek == 0 ? 6 : dayOfTheWeek - 1));
+    return new Date(newDate);
+}
+
+const upcomingSunday = (dateObject) => {
+    const today = dateObject.getDate();
+    const dayOfTheWeek = dateObject.getDay();
+    const newDate = dateObject.setDate(today + (dayOfTheWeek == 0 ? 0 : 7 - dayOfTheWeek));
+    return new Date(newDate);
+}
+
 const isBetween = (date, min, max) => (date.getTime() >= min.getTime() && date.getTime() <= max.getTime());
 const checkAvailability = (roomId, startTime, endTime) => {
     const dateStart = new Date(startTime);
@@ -84,7 +106,28 @@ const checkAvailability = (roomId, startTime, endTime) => {
     }
     return true;
  }
+
+const getBookingsForWeek = (roomId , nextWeek = false) => {
+    let currDay = new Date();
+    if(nextWeek) {
+        currDay.setDate(currDay.getDate() + 7);
+    }
+    const monday = lastMonday(currDay);
+    // console.log('monday' , monday);
+    const sunday = upcomingSunday(currDay);
+    // console.log('sunday' , sunday);
+    const bookingToday = bookingData.filter((item)=> {
+        const startTime = new Date(item.startTime);
+        // console.log('starttime' , startTime)
+        return item.roomId === roomId && isBetween(startTime , monday , sunday);
+    })
+    return bookingToday;
+}
+
+
+
  describe('Room available', () => {
+
     test('A101 not available', () => {
         const available = checkAvailability('A101' , '2019-09-28 13:10' , '2019-09-28 13:30');
         expect(available).toBe(false);
@@ -104,4 +147,26 @@ const checkAvailability = (roomId, startTime, endTime) => {
         const available = checkAvailability('A102' , '2020-09-28 13:10' , '2020-09-28 13:30');
         expect(available).toBe(true);
     })
+
+    test('Booking A101 in today is empty', () =>  {
+        const bookings = getBookingsForWeek('A101');
+        const today = new Date();
+        const currentRoomBooking = bookings.filter((item)=>{
+            const startTime = new Date(item.startTime);
+            const endTime = new Date(item.endTime);
+            return item.roomId === 'A101' && isBetween(today , startTime , endTime)
+        })
+        expect(currentRoomBooking).toHaveLength(0);
+    })
+
+    test('Booking A101 in this week is empty', () =>  {
+        const bookings = getBookingsForWeek('A101');
+        expect(bookings).toHaveLength(1);
+    })
+
+    test('Booking A101 in next week is empty', () =>  {
+        const bookings = getBookingsForWeek('A101' , true);
+        expect(bookings).toHaveLength(0);
+    })
+
  })
